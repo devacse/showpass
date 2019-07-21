1020e64e35cf95b5c89ab070e550a7f8abc48334
Dev <neyacse@gmail.com>
2019-07-21 22:16:56 +0530

    Added Show password link as a child for password field

--- showpass.js
+++ showpass.js
@@ -22,7 +22,7 @@
   function showPasswords(){
      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
        chrome.tabs.sendMessage(tabs[0].id, {method: 'showPasswords'}, function(res) {
-
+        console.log(res);
       });
     });
   }
